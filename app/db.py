e38e4d2d1f1f15affd23f912b90669ad3bf5cfa0
alextwtp <alextwtp@gmail.com>
2026-09-03 15:13:26 +0800

    feat(security): integrate AWS Secrets Manager for DB credentials

--- app/db.py
+++ app/db.py
@@ -2,15 +2,18 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from test_secret import get_db_secret
 
-load_dotenv(override=False)
+# Get AWS Secrets Manager credentials
+db_config = get_db_secret()
 
+get_db_secret()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = db_config["DB_USER"]
+DB_PASSWORD = db_config["DB_PASSWORD"]
+DB_HOST = db_config["DB_HOST"]
+DB_PORT = db_config["DB_PORT"]
+DB_NAME = db_config["DB_NAME"]
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
